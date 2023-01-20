d62b2e838780c023eed527202f3bd159195ec649
nelson-sci-labs <85094353+nelson-sci-labs@users.noreply.github.com>
2023-01-20 11:15:20 -0500

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -21,7 +21,7 @@ app_ui <- function(request) {
       title = "ShinyLink",
       # Header -----------------------------------------------------------------
       header = dashboardHeader(
-        title = "ShinyLink",
+        title = "ShinyLink Ver 0.2.3",
         leftUi = tagList(
           dropdownBlock(
             id = "mydropdown",
